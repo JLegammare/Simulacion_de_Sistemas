9492642a61c75b654f08f8a57275d5f476164293
JOAQUÍN EZEQUIEL LEGAMMARE <jlegammare@itba.edu.ar>
2022-08-08 11:56:13 -0300

    refactor: parserArguments implementation

--- TP1/TP1_java/src/main/java/CellIndexMethod.java
+++ TP1/TP1_java/src/main/java/CellIndexMethod.java
@@ -5,76 +5,35 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class CellIndexMethod {
-    //N: cantidad de particulas
-    private static int N = 10;
-    //N_ITERATIONS: cantidad de iteraciones en el tiempo
-    private static final int N_ITERATIONS = 1;
-//    R: radio de la particula
-    private static final double R= 0.37;
-//    L: largo de una celda
-    private static int L;
-//    M: Cantidad de celdas del board cuadrado (MxM)
-    private static final int M= 6;
-//    RC: Radio de interaccion
-    private static final int RC = 5;
-    private static final String STATIC_FILE = "TP1/TP1_java/assets/Static100.txt";
-    private static final String DYNAMIC_FILE = "TP1/TP1_java/assets/Dynamic100.txt";
-    private static final String OUTPUT_FILE = "TP1/TP1_java/assets/Output.txt";
 
     public static void main(String[] args) throws IOException {
-
-        Options options = new Options();
-
-        Option dynamicInput = new Option("d", "dynamic-input", true, "dynamic input file path");
-        dynamicInput.setRequired(true);
-        options.addOption(dynamicInput);
-
-        Option staticInput = new Option("s", "static-input", true, "static input file path");
-        staticInput.setRequired(true);
-        options.addOption(staticInput);
-
-        Option output = new Option("o", "output", true, "output file path");
-        output.setRequired(true);
-        options.addOption(output);
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-
-        try{
-            cmd = parser.parse(options,args);
-
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("utility-name",options);
-            throw new RuntimeException(e);
-        }
+        Parser parser = new Parser();
+        CommandLine cmd = parser.parseArguments(args);
 
         String staticInputFilePath = cmd.getOptionValue("static-input");
         String dynamicInputFilePath = cmd.getOptionValue("dynamic-input");
         String outputFilePath = cmd.getOptionValue("output");
+        int n = Integer.parseInt(cmd.getOptionValue("n_particles"));
+        int l = Integer.parseInt(cmd.getOptionValue("length"));
+        int rc = Integer.parseInt(cmd.getOptionValue("i_radius"));
 
+        List<Particle> particles = parser.parseParticles(staticInputFilePath,dynamicInputFilePath);
 
-        List<Particle> particles = Parser.parseParticles(staticInputFilePath,dynamicInputFilePath);
+        if (particles.size() != n){
+            //TODO: Throw exception and show a message
+            System.exit(1);
+        }
 
         Map<Particle,List<Particle>> neighborsMap = new HashMap<>();
 
         for (Particle p: particles) {
             neighborsMap.put(p,particles.stream().filter(particle ->
-                particle.checkIfNeighbor(p,RC)
+                particle.checkIfNeighbor(p,rc)
             ).collect(Collectors.toList()));
         }
 
-       Parser.generateOutput(neighborsMap,outputFilePath);
+       parser.generateOutput(neighborsMap,outputFilePath);
 
     }
 
-
-      public static void setL(int l) {
-        L = l;
-    }
-
-    public static void setN(int n) {
-        N = n;
-    }
 }
--- TP1/TP1_java/src/main/java/Parser.java
+++ TP1/TP1_java/src/main/java/Parser.java
@@ -1,3 +1,5 @@
+import org.apache.commons.cli.*;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,18 +8,16 @@ import java.util.Scanner;
 
 public class Parser {
 
-    public static List<Particle> parseParticles(String staticFilePath, String dynamicFilePath) throws IOException {
+    public List<Particle> parseParticles(String staticFilePath, String dynamicFilePath) throws IOException {
 
         File staticFile = new File(staticFilePath);
         File dynamicFile = new File(dynamicFilePath);
 
-        Scanner staticScanner = new Scanner(new File(staticFilePath));
-        Scanner dynamicScanner = new Scanner(new File(dynamicFilePath));
+        Scanner staticScanner = new Scanner(staticFile);
+        Scanner dynamicScanner = new Scanner(dynamicFile);
 
         int N = Integer.parseInt(staticScanner.next());
         int L = Integer.parseInt(staticScanner.next());
-        CellIndexMethod.setL(L);
-        CellIndexMethod.setN(N);
         ArrayList<Integer> times= new ArrayList<>();
         times.add(dynamicScanner.nextInt());
         List<Particle> particles = new ArrayList<>();
@@ -33,30 +33,77 @@ public class Parser {
         return particles;
     }
 
-     public static  void generateOutput(Map<Particle,List<Particle>> neighbors, String path) throws IOException {
+    public void generateOutput(Map<Particle,List<Particle>> neighbors, String path) throws IOException {
 
-            File file = new File(path);
-            if(!file.exists()){
-                file.createNewFile();
-            }
+        File file = new File(path);
+        if(!file.exists()){
+            file.createNewFile();
+        }
 
         FileWriter fw = new FileWriter(file,false);
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter pw = new PrintWriter(bw);
 
         neighbors.forEach((particle, particles) -> {
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("[ ");
-                    sb.append(particle.getId());
-                    for (Particle p: particles) {
-                        sb.append(String.format(" %d",p.getId()));
-                    }
-                     sb.append(" ]");
-                     pw.println(sb);
+            StringBuilder sb = new StringBuilder();
+            sb.append("[ ");
+            sb.append(particle.getId());
+            for (Particle p: particles) {
+                sb.append(String.format(" %d",p.getId()));
+            }
+            sb.append(" ]");
+            pw.println(sb);
 
         });
 
         pw.close();
+    }
+
+    public CommandLine parseArguments(String[] args){
+        Options options = new Options();
+
+        Option dynamicInput = new Option("d", "dynamic-input", true, "dynamic input file path");
+        dynamicInput.setRequired(true);
+        options.addOption(dynamicInput);
+
+        Option staticInput = new Option("s", "static-input", true, "static input file path");
+        staticInput.setRequired(true);
+        options.addOption(staticInput);
+
+        Option output = new Option("o", "output", true, "output file path");
+        output.setRequired(true);
+        options.addOption(output);
+
+        Option n = new Option("n", "n_particles", true, "number of particles");
+        n.setRequired(true);
+        options.addOption(n);
+
+        Option l = new Option("l", "length", true, "length of the cells");
+        l.setRequired(true);
+        options.addOption(l);
+
+        Option m = new Option("m", "m_cells", true, "number of cells");
+        m.setRequired(true);
+        options.addOption(m);
+
+        Option rc = new Option("rc", "i_radius", true, "particle interaction radius");
+        rc.setRequired(true);
+        options.addOption(rc);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try{
+            cmd = parser.parse(options,args);
+
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name",options);
+            throw new RuntimeException(e);
+        }
+
+        return cmd;
     }
 
 
