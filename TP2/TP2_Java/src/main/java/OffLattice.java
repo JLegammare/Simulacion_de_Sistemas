efddba738b411a406d46753d3d1b76a7b6062805
JOAQUÍN EZEQUIEL LEGAMMARE <jlegammare@itba.edu.ar>
2022-08-21 19:57:35 -0300

    fix: validate condition

--- TP2/TP2_Java/src/main/java/OffLattice.java
+++ TP2/TP2_Java/src/main/java/OffLattice.java
@@ -7,42 +7,46 @@ import static java.lang.Math.*;
 
 public class OffLattice {
 
-   private static int DEFAULT_M = 4;
-   private static final int TOTAL_ITERATIONS = 100;
-   public static void main(String[] args) throws IOException {
-      Parser parser = new Parser();
-      CommandLine cmd = parser.parseArguments(args);
+    private static int DEFAULT_M = 4;
+    private static final int TOTAL_ITERATIONS = 100;
 
-      String staticInputFilePath = cmd.getOptionValue("static-input");
-      String dynamicInputFilePath = cmd.getOptionValue("dynamic-input");
-      String outputFilePath = cmd.getOptionValue("output");
+    public static void main(String[] args) throws IOException {
+        Parser parser = new Parser();
+        CommandLine cmd = parser.parseArguments(args);
 
-      double n = Double.parseDouble(cmd.getOptionValue("n_particles"));
-      int l = Integer.parseInt(cmd.getOptionValue("length"));
-      double rc = Float.parseFloat(cmd.getOptionValue("i_radius"));
-      int m = DEFAULT_M;
-      boolean periodicCondition = true;
+        String staticInputFilePath = cmd.getOptionValue("static-input");
+        String dynamicInputFilePath = cmd.getOptionValue("dynamic-input");
+        String outputFilePath = cmd.getOptionValue("output");
 
-      List<Particle> particles = ParticleGenerator.generateRandomParticles(100,m,l,n,0.1,0.03);
-      Board board = new Board(m, l, periodicCondition);
-      board.addParticlesToBoard(particles);
+        double n = Double.parseDouble(cmd.getOptionValue("n_particles"));
+        double l = Double.parseDouble(cmd.getOptionValue("length"));
+        double rc = Double.parseDouble(cmd.getOptionValue("i_radius"));
+        int m = 9;
 
-      parser.addIterationToOutput(0,particles,outputFilePath);
-      List<Double> orderParameterList = new ArrayList<>();
+        validateParams(m,l,rc);
 
-      for (int i = 1; i < TOTAL_ITERATIONS; i++) {
-         Map<Particle, Set<Particle>> neighborhoods = board.getAllNeighbors(rc);
-         double va = calculateOrderParameter(particles,0.03);
-         orderParameterList.add(va);
-         particles.forEach(p->tempEvolution(p, neighborhoods.get(p),l, m));
-         board.addParticlesToBoard(particles);
-         parser.addIterationToOutput(i,particles,outputFilePath);
+        boolean periodicCondition = true;
 
-      }
+        List<Particle> particles = ParticleGenerator.generateRandomParticles(100, m, l, n, 0.1, 0.03);
+        Board board = new Board(m, l, periodicCondition);
+        board.addParticlesToBoard(particles);
 
-   }
+        parser.addIterationToOutput(0, particles, outputFilePath);
+        List<Double> orderParameterList = new ArrayList<>();
 
-   private static double calculateOrderParameter(List<Particle> particles, double v) {
+        for (int i = 1; i < TOTAL_ITERATIONS; i++) {
+            Map<Particle, Set<Particle>> neighborhoods = board.getAllNeighbors(rc);
+            double va = calculateOrderParameter(particles, 0.03);
+            orderParameterList.add(va);
+            particles.forEach(p -> tempEvolution(p, neighborhoods.get(p), l, m));
+            board.addParticlesToBoard(particles);
+            parser.addIterationToOutput(i, particles, outputFilePath);
+
+        }
+
+    }
+
+    private static double calculateOrderParameter(List<Particle> particles, double v) {
 
         double vx = particles.stream().mapToDouble(Particle::getXVelocity).sum();
         double vy = particles.stream().mapToDouble(Particle::getYVelocity).sum();
@@ -50,65 +54,60 @@ public class OffLattice {
 
         return norm / (particles.size() * v);
 
-   }
+    }
 
-   private static int optimizedM(int l, int n, double rc, double maxRadius){
+    private static void validateParams(int m,double l, double rc ) {
+        if (l / m <= rc) {
+            throw new RuntimeException("CONDITION IS NOT SATISFIED!");
+        }
+    }
 
-      int m = (int) (l/(rc + 2*maxRadius) + 1);
-      if((double)l/m <= rc ){
-         System.exit(1);
-      }
+    private static void tempEvolution(Particle particle, Set<Particle> neighbors, double l, int m) {
 
-      return m;
-   }
+        double sinAvg = 0;
+        double cosAvg = 0;
+        double omegaAvg;
+        double boardLength = m * l;
 
-   private static void tempEvolution(Particle particle, Set<Particle> neighbors, int l, int m){
+        if (!neighbors.isEmpty()) {
+            for (Particle n : neighbors) {
+                sinAvg += sin(n.getOmega());
+                cosAvg += cos(n.getOmega());
+            }
+            sinAvg += sin(particle.getOmega());
+            cosAvg += cos(particle.getOmega());
 
-      double sinAvg = 0;
-      double cosAvg = 0;
-      double omegaAvg;
-      int boardLength = m*l;
+            sinAvg = sinAvg / (neighbors.size() + 1);
+            cosAvg = cosAvg / (neighbors.size() + 1);
 
-      if(!neighbors.isEmpty()) {
-         for (Particle n : neighbors) {
-            sinAvg += sin(n.getOmega());
-            cosAvg += cos(n.getOmega());
-         }
-         sinAvg += sin(particle.getOmega());
-         cosAvg += cos(particle.getOmega());
+            omegaAvg = atan2(sinAvg, cosAvg);
+            particle.setOmega(omegaAvg + particle.getDeltaOmega());
+        }
 
-         sinAvg = sinAvg / (neighbors.size() + 1);
-         cosAvg = cosAvg / (neighbors.size() + 1);
-
-         omegaAvg = atan2(sinAvg, cosAvg);
-         particle.setOmega(omegaAvg + particle.getDeltaOmega());
-      }
-
-      double newXposition = particle.getX() + particle.getXVelocity();
-      double newYposition = particle.getY() + particle.getYVelocity();
+        double newXposition = particle.getX() + particle.getXVelocity();
+        double newYposition = particle.getY() + particle.getYVelocity();
 
 
-      //si se va por la derecha
-      if(newXposition > boardLength){
-         particle.setX(newXposition - boardLength);
-      }//si se va por izquierda
-      else if(newXposition < 0)
-         particle.setX(boardLength + newXposition);
-      else
-         particle.setX(newXposition);
-      //si se va por arriba
-      if(newYposition > boardLength){
-         particle.setY(newYposition - boardLength);
-      }//si se va por abajo
-      else if(newYposition < 0)
-         particle.setY(boardLength + newYposition);
-      else
-         particle.setY(newYposition);
+        //si se va por la derecha
+        if (newXposition > boardLength) {
+            particle.setX(newXposition - boardLength);
+        }//si se va por izquierda
+        else if (newXposition < 0)
+            particle.setX(boardLength + newXposition);
+        else
+            particle.setX(newXposition);
+        //si se va por arriba
+        if (newYposition > boardLength) {
+            particle.setY(newYposition - boardLength);
+        }//si se va por abajo
+        else if (newYposition < 0)
+            particle.setY(boardLength + newYposition);
+        else
+            particle.setY(newYposition);
 
-      particle.updateOmega(PI);
+        particle.updateOmega(PI);
 
-   }
-
+    }
 
 
 }
