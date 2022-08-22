44f09a1f3155f44a067e48ffc4394898db79314f
Joaquin Legammare <jlegammare@gmail.com>
2022-08-22 14:48:41 -0300

    fix: typo error

--- TP2/TP2_Java/src/main/java/ResultsGenerator.java
+++ TP2/TP2_Java/src/main/java/ResultsGenerator.java
@@ -5,7 +5,7 @@ public class ResultsGenerator {
 
     private final File dynamicFile;
     private File vaTimeFile;
-    private File staticFile;
+    private final File staticFile;
 
     public ResultsGenerator(String dynamicFilePath,String vaFilePath, String staticFilePath) {
 
@@ -14,12 +14,12 @@ public class ResultsGenerator {
             dymFile.delete();
         this.dynamicFile= dymFile;
 
-        File stcFile = new File(dynamicFilePath);
+        File stcFile = new File(staticFilePath);
         if(stcFile.exists())
             stcFile.delete();
         this.staticFile= stcFile;
 
-        File vaFile = new File(dynamicFilePath);
+        File vaFile = new File(vaFilePath);
         if(vaFile.exists())
             vaFile.delete();
         this.vaTimeFile=vaFile;
@@ -28,18 +28,20 @@ public class ResultsGenerator {
 
     public void fillStaticFile(List<Particle> particles,double l) throws IOException {
 
-        FileWriter fw = new FileWriter(staticFile,true);
+        FileWriter fw = new FileWriter(staticFile,false);
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter pw = new PrintWriter(bw);
+
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("%d\n%f\n",particles.size(),l));
         for (Particle p: particles) {
             sb.append(String.format("%f %f\n",p.getRadius(), p.getRadius()));
         }
         pw.println(sb);
+        pw.close();
     }
 
-    public void addStateToDynamicFile( List<Particle> particles) throws IOException {
+    public void addStateToDynamicFile(List<Particle> particles) throws IOException {
 
         FileWriter fw = new FileWriter(dynamicFile,true);
         BufferedWriter bw = new BufferedWriter(fw);
