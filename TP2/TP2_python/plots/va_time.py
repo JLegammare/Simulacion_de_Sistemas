06721d1c1301124ea41da39fae125e7d91ed8e7d
JOAQUÍN EZEQUIEL LEGAMMARE <jlegammare@itba.edu.ar>
2022-08-29 00:20:57 -0300

    feat: new scientific notation format

--- TP2/TP2_python/plots/va_time.py
+++ TP2/TP2_python/plots/va_time.py
@@ -9,14 +9,16 @@ def main(config: Config):
 
     dfp = pd.read_csv(config.va_time_file, skiprows=1, sep=" ", names=["times","order_parameters"])
     df = pd.DataFrame(dict(
-        iteraciones=dfp['times'],
+        Tiempo=dfp['times'],
         Va=dfp['order_parameters']
     ))
-    fig = px.line(df, x="iteraciones", y="Va")
+    fig = px.line(df, x="Tiempo", y="Va")
     data = list(range(0,20000+1,5000))
-    tickvals = np.array(data,dtype=float)
+    tickvals = np.array(data)
 
     ticktext = [format_tick(t) for t in tickvals]
+
+    ticktext[0]= "0"
 
     fig.update_layout(
         xaxis = dict(
@@ -31,10 +33,18 @@ def main(config: Config):
         yaxis_range = [0,1]
     )
     fig.show()
-def format_tick(n: float):
+def format_tick(n: int):
         m, e = f"{n:.0e}".split("e")
+        m = n/_get_dec(n)
         e = int(e)
         return f"{m} x 10<sup>{e}</sup>"
+
+def _get_dec(n:int):
+    dec=10
+    while n>=dec:
+        dec=dec*10
+    return dec/10
+
 
 
 if __name__ == '__main__':
