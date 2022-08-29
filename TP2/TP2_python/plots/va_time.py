e76d17d7410502a39e290948ed869ffc6c85073f
JOAQUÍN EZEQUIEL LEGAMMARE <jlegammare@itba.edu.ar>
2022-08-28 23:22:43 -0300

    feat: new va vs time plot axis

--- TP2/TP2_python/plots/va_time.py
+++ TP2/TP2_python/plots/va_time.py
@@ -1,3 +1,4 @@
+import numpy as np
 import plotly.express as px
 import pandas as pd
 
@@ -8,11 +9,32 @@ def main(config: Config):
 
     dfp = pd.read_csv(config.va_time_file, skiprows=1, sep=" ", names=["times","order_parameters"])
     df = pd.DataFrame(dict(
-        x=dfp['times'],
-        y=dfp['order_parameters']
+        iteraciones=dfp['times'],
+        Va=dfp['order_parameters']
     ))
-    fig = px.line(df, x="x", y="y", title="Order Parameter vs Time")
+    fig = px.line(df, x="iteraciones", y="Va")
+    data = list(range(0,20000+1,5000))
+    tickvals = np.array(data,dtype=float)
+
+    ticktext = [format_tick(t) for t in tickvals]
+
+    fig.update_layout(
+        xaxis = dict(
+            tickmode = 'array',
+            tickvals = tickvals,
+            ticktext = ticktext,
+        ),
+        font=dict(
+            family="Arial",
+            size=40,
+            ),
+        yaxis_range = [0,1]
+    )
     fig.show()
+def format_tick(n: float):
+        m, e = f"{n:.0e}".split("e")
+        e = int(e)
+        return f"{m} x 10<sup>{e}</sup>"
 
 
 if __name__ == '__main__':
