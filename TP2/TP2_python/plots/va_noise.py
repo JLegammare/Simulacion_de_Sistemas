176d5c27c8a43cc448498f73b27b698bd014aaf7
JOAQUÍN EZEQUIEL LEGAMMARE <jlegammare@itba.edu.ar>
2022-08-29 13:20:10 -0300

    feat: va vs noise plot

--- TP2/TP2_python/plots/va_noise.py
+++ TP2/TP2_python/plots/va_noise.py
@@ -1,20 +1,57 @@
-import plotly.express as px
+import statistics
 import pandas as pd
+import plotly.graph_objects as go
 
-from models.config import Config
-from utils.parser import parse_arguments
+def main():
 
-def main(config: Config):
+    inc = 0.1
+    densidad = 1
+    vias = 5000
 
-    dfp = pd.read_csv(config.va_time_file, skiprows=1, sep=" ", names=["times","order_parameters"])
-    df = pd.DataFrame(dict(
-        x=dfp['times'],
-        y=dfp['order_parameters']
-    ))
-    fig = px.line(df, x="x", y="y", title="Order Parameter vs Time")
+    data = []
+    x = []
+    avgs = []
+    stdevs_list = []
+
+    for i in range(0,50+1):
+
+        x.append(i*0.1)
+        dfp = pd.read_csv("./va_time_files/VaTime{it}.txt".format(it=i), skiprows=1, sep=" ", names=["times","order_parameters"])
+        l= list(dfp.get("order_parameters"))
+        end = len(l)
+        its_list = l[vias:end]
+        avgs.append(sum(its_list)/len(its_list))
+        stdevs_list.append(statistics.stdev(its_list))
+
+    data.append(
+        go.Scatter(
+            x=x,
+                y=avgs,
+                mode='markers+lines',
+                error_y=dict(array=stdevs_list)
+            )
+        )
+
+    fig = go.Figure(
+        data = data,
+        layout= go.Layout(
+            xaxis=dict(title='Ruido'),
+            yaxis=dict(title='Va Promedio'),
+            font=dict(
+                family="Arial",
+                size=22,
+            ),
+            legend=dict(
+                yanchor="top",
+                y=0.99,
+                xanchor="right",
+                x=0.99
+            )
+        )
+    )
+
     fig.show()
 
 
 if __name__ == '__main__':
-    conf = parse_arguments()
-    main(conf)
+    main()
