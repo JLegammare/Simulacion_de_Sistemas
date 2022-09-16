bebf75b665d22d3781a216437e669b91969082d9
Santaigo Preusche <spreusche@itba.edu.ar>
2022-09-16 20:09:46 -0300

    big particle trajectory and pdf collision freq plots

--- TP3/TP3_python/plots/big_particle_trajectory.py
+++ TP3/TP3_python/plots/big_particle_trajectory.py
@@ -7,62 +7,165 @@ def big_particle_trajectory(big_particle_v1: str, big_particle_v2: str, big_part
     time1 = []
     x_pos1 = []
     y_pos1 = []
+    total_time1 = 0
 
     big_particle_position_file_v2 = open(big_particle_v2)
     lines2 = big_particle_position_file_v2.readlines()
     time2 = []
     x_pos2 = []
     y_pos2 = []
+    total_time2 = 0
 
     big_particle_position_file_v3 = open(big_particle_v3)
     lines3 = big_particle_position_file_v3.readlines()
     time3 = []
     x_pos3 = []
     y_pos3 = []
+    total_time3 = 0
 
     for line in lines1:
         values = line.split(" ")
-        time1.append(float(values[0]))
-        x_pos1.append(float(values[1]))
-        y_pos1.append(float(values[2]))
+        total_time1 += float(values[0])
 
     for line2 in lines2:
         values = line2.split(" ")
-        time2.append(float(values[0]))
-        x_pos2.append(float(values[1]))
-        y_pos2.append(float(values[2]))
+        total_time2 += float(values[0])
 
     for line3 in lines3:
         values = line3.split(" ")
-        time3.append(float(values[0]))
-        x_pos3.append(float(values[1]))
-        y_pos3.append(float(values[2]))
+        total_time3 += float(values[0])
+
+
+    total_time_min = min(total_time1, total_time2, total_time3)
+
+    print(total_time_min)
+    print("---")
+    print(total_time1)
+    print(total_time2)
+    print(total_time3)
+
+
+
+    total_time = 0
+    for line in lines1:
+        values = line.split(" ")
+        time = float(values[0])
+        total_time += time
+        if total_time <= total_time_min:
+            time1.append(time)
+            x_pos1.append(float(values[1]))
+            y_pos1.append(float(values[2]))
+
+    total_time = 0
+    for line2 in lines2:
+        values = line2.split(" ")
+        time = float(values[0])
+        total_time += time
+        if total_time <= total_time_min:
+            time2.append(time)
+            x_pos2.append(float(values[1]))
+            y_pos2.append(float(values[2]))
+
+    total_time = 0
+    for line3 in lines3:
+        values = line3.split(" ")
+        time = float(values[0])
+        total_time += time
+        if total_time <= total_time_min:
+            time3.append(time)
+            x_pos3.append(float(values[1]))
+            y_pos3.append(float(values[2]))
+
 
     data = [go.Scatter(
         x=x_pos1[:],
         y=y_pos1[:],
-        name="v=[0, 2] m/s"
+        line=dict(color="Green"),
+        name="v=[0, 2] m/s",
     ), go.Scatter(
         x=x_pos2[:],
         y=y_pos2[:],
+        line=dict(color="Blue"),
         name="v=[2, 4] m/s"
     ), go.Scatter(
         x=x_pos3[:],
         y=y_pos3[:],
+        line=dict(color="Red"),
         name="v=[4, 6] m/s"
     )]
 
     fig = go.Figure(
         data=data,
         layout=go.Layout(
-            xaxis=dict(title='X', range=[0, 6]),
-            yaxis=dict(title='Y', range=[0, 6]),
+            xaxis=dict(title='X (m)', range=[0, 6], exponentformat="power", showgrid=False, linecolor='black', ticks='inside'),
+            yaxis=dict(title='Y (m)', range=[0, 6], exponentformat="power", showgrid=False, linecolor='black', ticks='inside'),
             font=dict(
                 family="Arial",
                 size=23,
-            )
+            ),
+            plot_bgcolor='white',
         )
     )
+
+    last_x_pos1 = x_pos1[-1]
+    last_y_pos1 = y_pos1[-1]
+    x0 = last_x_pos1 - 0.7
+    x1 = last_x_pos1 + 0.7
+    y0 = last_y_pos1 - 0.7
+    y1 = last_y_pos1 + 0.7
+
+    fig.add_shape(type="circle",
+                  xref="x", yref="y",
+                  x0=x0, y0=y0, x1=x1, y1=y1,
+                  opacity=0.2,
+                  line_color="Green",
+                  fillcolor="Green"
+                  )
+
+    last_x_pos2 = x_pos2[-1]
+    last_y_pos2 = y_pos2[-1]
+    x0 = last_x_pos2 - 0.7
+    x1 = last_x_pos2 + 0.7
+    y0 = last_y_pos2 - 0.7
+    y1 = last_y_pos2 + 0.7
+
+    fig.add_shape(type="circle",
+                  xref="x", yref="y",
+                  x0=x0, y0=y0, x1=x1, y1=y1,
+                  opacity=0.2,
+                  line_color="Blue",
+                  fillcolor="Blue"
+                  )
+
+    last_x_pos3 = x_pos3[-1]
+    last_y_pos3 = y_pos3[-1]
+    x0 = last_x_pos3 - 0.7
+    x1 = last_x_pos3 + 0.7
+    y0 = last_y_pos3 - 0.7
+    y1 = last_y_pos3 + 0.7
+
+    fig.add_shape(type="circle",
+                  xref="x", yref="y",
+                  x0=x0, y0=y0, x1=x1, y1=y1,
+                  opacity=0.2,
+                  line_color="Red",
+                  fillcolor="Red"
+                  )
+
+    fig.add_shape(
+            # Rectangle with reference to the plot
+            type="rect",
+            xref="paper",
+            yref="paper",
+            x0=0,
+            y0=0,
+            x1=1.0,
+            y1=1.0,
+            line=dict(
+                color="black",
+                width=1,
+            )
+         )
 
     fig.update_layout(width=1000, height=1000)
 
@@ -96,5 +199,20 @@ if __name__ == "__main__":
 # ---------------->
 #
 # CollisionTime.txt
+
+# fig.add_shape(
+    #    # Rectangle with reference to the plot
+    #    type="rect",
+    #    xref="paper",
+    #    yref="paper",
+    #    x0=0,
+    #    y0=0,
+    #    x1=1.0,
+    #    y1=1.0,
+    #    line=dict(
+    #        color="black",
+    #        width=1,
+    #    )
+    # )
 
 
--- TP3/TP3_python/plots/pdf_collision_frequency_plot.py
+++ TP3/TP3_python/plots/pdf_collision_frequency_plot.py
@@ -1,36 +1,49 @@
 import numpy as np
 import plotly.graph_objects as go
 
-def pdf_collision_frequency(collisions_time_path: str):
-    collisions_time = open(collisions_time_path)
-    lines = collisions_time.readlines()
-    total_time = 0
-    collisions = []
-    avg_cols_times = []
 
-    for line in lines:
-        total_time += float(line)
-        collisions.append(float(line))  #
+def pdf_collision_frequency(collisions_time_path1: str, collisions_time_path2: str, collisions_time_path3: str, collisions_time_path4: str):
+    paths = [collisions_time_path1, collisions_time_path2, collisions_time_path3, collisions_time_path4]
+    data = []
+    j = 110
 
-    avg = (len(lines)-1)/ total_time
-    avg_cols_time = np.mean(collisions)
-    standard_deviation = np.std(collisions)
+    for i in paths:
+        collisions_time = open(i)
+        lines = collisions_time.readlines()
+        total_time = 0
+        collisions = []
+        avg_cols_times = []
 
+        for line in lines:
+            total_time += float(line)
+            collisions.append(float(line))
+
+        avg = (len(lines) - 1) / total_time
+        avg_cols_time = np.mean(collisions)
+        standard_deviation = np.std(collisions)
+
+        hist, bin_edges = np.histogram(collisions, bins=40, density=True)
+        bin_centres = (bin_edges[:-1] + bin_edges[1:]) / 2.
+
+        data.append(go.Scatter(
+            x=bin_centres,
+            y=hist,
+            mode='lines',
+            name='N=' + str(j)
+        ))
+        j += 10
 
     fig = go.Figure(
-        data=go.Histogram(
-            x=collisions,
-            histnorm="probability density",
-            nbinsx=40
-        ),
+        data=data,
         layout=go.Layout(
-            title=dict(text='Collision Times'),
-            xaxis=dict(title='Time (s)'),
-            yaxis=dict(title='Probability Density Function'),
+            xaxis=dict(title='Tiempo (s)', exponentformat="power", showgrid=False, linecolor='black', ticks='inside'),
+            yaxis=dict(title='PDF', exponentformat="power", showgrid=False, linecolor='black', ticks='inside'),
             font=dict(
                 family="Arial",
-                size=23,
-            )
+                size=22,
+            ),
+            plot_bgcolor='white',
+
         )
     )
 
@@ -38,7 +51,9 @@ def pdf_collision_frequency(collisions_time_path: str):
     fig.show()
 
 
-
 if __name__ == "__main__":
-    collisions_time_path = './simulation_results/CollisionsTime.txt'
-    pdf_collision_frequency(collisions_time_path)
+    collisions_time_path1 = '../../../simulation_results/110/CollisionsTime.txt'
+    collisions_time_path2 = '../../../simulation_results/120/CollisionsTime.txt'
+    collisions_time_path3 = '../../../simulation_results/130/CollisionsTime.txt'
+    collisions_time_path4 = '../../../simulation_results/140/CollisionsTime.txt'
+    pdf_collision_frequency(collisions_time_path1, collisions_time_path2, collisions_time_path3, collisions_time_path4)
