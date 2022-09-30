7898ca834c542791020acb3b57e08c9bdf7d65eb
JOAQUÍN EZEQUIEL LEGAMMARE <jlegammare@itba.edu.ar>
2022-09-30 12:19:35 -0300

    refactor: HarmonicOscillator

--- TP4/TP4_java/src/main/java/ar/edu/itba/simulacion/HarmonicOscillator.java
+++ TP4/TP4_java/src/main/java/ar/edu/itba/simulacion/HarmonicOscillator.java
@@ -2,23 +2,27 @@ package ar.edu.itba.simulacion;
 
 import ar.edu.itba.simulacion.methods.Beeman;
 import ar.edu.itba.simulacion.methods.Euler;
+import ar.edu.itba.simulacion.methods.GearPredictoO5;
+import ar.edu.itba.simulacion.methods.OriginalVerlet;
 import ar.edu.itba.simulacion.models.Pair;
 import ar.edu.itba.simulacion.utils.ResultsGenerator;
 
 import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class HarmonicOscillator {
 
     //parameters
-    private static final String RESULTS_DIRECTORY =  "simulation_results";
+    private static final String RESULTS_DIRECTORY = "simulation_results";
     private static final String POSITION_FILE = "Position.txt";
     private static final double M = 70.0; //kg
     private static final double k = Math.pow(10.0, 4); //N/mts
     private static final double gamma = 100.0; //kg/s
     private static final double tf = 5.0; //s
     private static final double DT = 0.002;
-    //initial conditions
+    private static final String Met = "GEAR";
     private static final double r0 = 1.0; //mts
     private static final double v0 = -gamma / (2 * M); //mts/s
     private static final Pair<Double, Double> initPosition = new Pair<>(r0, 0.0);
@@ -36,115 +40,169 @@ public class HarmonicOscillator {
 
     public static void main(String[] args) throws IOException {
 
-        String positionsResultsFilePath = String.format("%s/%s", RESULTS_DIRECTORY, POSITION_FILE);
-
-        ResultsGenerator rg = new ResultsGenerator(
-                positionsResultsFilePath,
-                RESULTS_DIRECTORY);
-
-
-
-        //BEEMAN:
 
         ArrayList<Pair<Double, Double>> positions = new ArrayList<>();
         ArrayList<Pair<Double, Double>> velocities = new ArrayList<>();
         ArrayList<Pair<Double, Double>> accelerations = new ArrayList<>();
+
+        Pair<Double, Double> prevPos = Euler.prevPosition(initPosition, initVelocity, initForce, -DT, M);
+        Pair<Double, Double> prevVel = Euler.prevVel(initVelocity, initForce, -DT, M);
+        Pair<Double, Double> prevAcc = new Pair<>(
+                        calcForce(prevPos.getX_value(), prevVel.getX_value()) / M,
+                        0.0);
+
         ArrayList<Double> times = new ArrayList<>();
+        String positionsResultsFilePath;
+        ResultsGenerator rg = null;
 
-        Pair<Double,Double> prevPos = Euler.prevPosition(initPosition, initVelocity, initForce,-DT,M);
-        Pair<Double,Double> prevVel = Euler.prevVel(initVelocity,initForce,-DT, M);
-        Pair<Double,Double> prevAcc = new Pair<>(
-                calcForce(prevPos.getX_value(),prevVel.getX_value())/M,
-                0.0);
+        switch (Met) {
 
-        positions.add(prevPos);
-        velocities.add(prevVel);
-        accelerations.add(prevAcc);
 
-        velocities.add(initVelocity);
-        positions.add(initPosition);
-        accelerations.add(initAcceleration);
+            case ("BEEMAN"):
 
-        BEEMAN:
+                positionsResultsFilePath = String.format("%s/Beeman%s", RESULTS_DIRECTORY, POSITION_FILE);
 
-        for (double t = 0; t <= tf; t += DT) {
-            int positionsSize = positions.size();
-            int accelerationSize = accelerations.size();
-            int velocitiesSize = velocities.size();
+                rg = new ResultsGenerator(
+                        positionsResultsFilePath,
+                        RESULTS_DIRECTORY);
 
-            Pair<Double, Double> currentPos = positions.get(positionsSize - 1);
-            Pair<Double, Double> currentAcc = accelerations.get(accelerationSize - 1);
-            Pair<Double, Double> previousAcc = accelerations.get(accelerationSize - 2);
-            Pair<Double, Double> currentVel = velocities.get(velocitiesSize - 1);
+                prevPos = Euler.prevPosition(initPosition, initVelocity, initForce, -DT, M);
+                prevVel = Euler.prevVel(initVelocity, initForce, -DT, M);
+                prevAcc = new Pair<>(
+                        calcForce(prevPos.getX_value(), prevVel.getX_value()) / M,
+                        0.0);
 
-            Pair<Double, Double> newPosition = Beeman.position(
-                    currentPos,
-                    currentVel,
-                    currentAcc,
-                    previousAcc,
-                    t
-            );
+                positions.add(prevPos);
+                velocities.add(prevVel);
+                accelerations.add(prevAcc);
 
-            Pair<Double, Double> newVel = Beeman.velocity(
-                    currentAcc,
-                    previousAcc,
-                    newPosition,
-                    DT,
-                    currentVel,
-                    M
-            );
+                velocities.add(initVelocity);
+                positions.add(initPosition);
+                accelerations.add(initAcceleration);
 
-            Pair<Double,Double> newAcc = new Pair<>(
-                    calcForce(newPosition.getX_value(),
-                            newVel.getX_value())/M,0.0);
-            accelerations.add(newAcc);
-            positions.add(newPosition);
-            velocities.add(newVel);
-            times.add(t);
+
+                for (double t = 0; t <= tf; t += DT) {
+                    int positionsSize = positions.size();
+                    int accelerationSize = accelerations.size();
+                    int velocitiesSize = velocities.size();
+
+                    Pair<Double, Double> currentPos = positions.get(positionsSize - 1);
+                    Pair<Double, Double> currentAcc = accelerations.get(accelerationSize - 1);
+                    Pair<Double, Double> previousAcc = accelerations.get(accelerationSize - 2);
+                    Pair<Double, Double> currentVel = velocities.get(velocitiesSize - 1);
+
+                    Pair<Double, Double> newPosition = Beeman.position(
+                            currentPos,
+                            currentVel,
+                            currentAcc,
+                            previousAcc,
+                            t
+                    );
+
+                    Pair<Double, Double> newVel = Beeman.velocity(
+                            currentAcc,
+                            previousAcc,
+                            newPosition,
+                            DT,
+                            currentVel,
+                            M
+                    );
+
+                    Pair<Double, Double> newAcc = new Pair<>(
+                            calcForce(newPosition.getX_value(),
+                                    newVel.getX_value()) / M, 0.0);
+                    accelerations.add(newAcc);
+                    positions.add(newPosition);
+                    velocities.add(newVel);
+                    times.add(t);
+                }
+                positions.remove(0);
+                rg.addStateToPositionFile(times, positions);
+
+                break;
+
+            case ("GEAR"):
+
+                positionsResultsFilePath = String.format("%s/Gear%s", RESULTS_DIRECTORY, POSITION_FILE);
+
+                rg = new ResultsGenerator(
+                        positionsResultsFilePath,
+                        RESULTS_DIRECTORY);
+
+                List<Double> rs;
+
+                prevPos = Euler.prevPosition(initPosition, initVelocity, initForce, -DT, M);
+                prevVel = Euler.prevVel(initVelocity, initForce, -DT, M);
+                prevAcc = new Pair<>(
+                        calcForce(prevPos.getX_value(), prevVel.getX_value()) / M,
+                        0.0);
+
+                positions.add(prevPos);
+                velocities.add(prevVel);
+                accelerations.add(prevAcc);
+
+                velocities.add(initVelocity);
+                positions.add(initPosition);
+                accelerations.add(initAcceleration);
+
+                for (double t = 0; t <= tf; t += DT) {
+
+                    int positionsSize = positions.size();
+                    int accelerationSize = accelerations.size();
+                    int velocitiesSize = velocities.size();
+
+
+                    Pair<Double, Double> currentPos = positions.get(positionsSize - 1);
+                    Pair<Double, Double> currentAcc = accelerations.get(accelerationSize - 1);
+                    Pair<Double, Double> previousAcc = accelerations.get(accelerationSize - 2);
+                    Pair<Double, Double> currentVel = velocities.get(velocitiesSize - 1);
+
+                    rs = GearPredictoO5.predict(
+                            currentPos,
+                            currentVel,
+                            currentAcc, DT, M);
+
+                    currentAcc = new Pair<>(rs.get(2), 0.0);
+                    accelerations.add(currentAcc);
+                    positions.add(new Pair<>(rs.get(0), 0.0));
+                    velocities.add(new Pair<>(rs.get(1), 0.0));
+                    times.add(t);
+                }
+
+                break;
+
+            case ("VERLET"):
+
+
+                //DUDA: POSITION ANTERIOR CON EULER ? YES OR NO ?
+
+
+//                Pair<Double, Double> prevVel = Euler.prevVel(initVelocity, new Pair<>(
+//                                calcForce(initPosition.getX_value(), initVelocity.getX_value()), 0.0),
+//                        DT,
+//                        M
+//                );
+//                Pair<Double, Double> prevPos = Euler.prevPosition(initPosition, initVelocity, new Pair<>(
+//                                calcForce(initPosition.getX_value(), initVelocity.getX_value()), 0.0),
+//                        DT,
+//                        m);
+//
+//                positions.add(0, prevPos);
+//
+//                for (double t = 0; t <= tf; t += DT) {
+//                    int size = positions.size();
+//                    positions.add(OriginalVerlet.position(positions, DT, new Pair<>(
+//                            calcForce(initPosition.getX_value(), initVelocity.getX_value()), 0.0), m));
+//                }
+
+                break;
+
+            default:
+                throw new InvalidObjectException("ENUM NOT FOUND");
+
         }
-        positions.remove(0);
-        rg.addStateToPositionFile(times,positions);
 
-//        GEAR PREDICT O5
-//
-//        Pair <Double,Double> currentAcc = initAcceleration;
-//
-//        List<Double> rs;
-//
-//        for (double t = 0; t <=tf ; t+=DT) {
-//            int size = positions.size();
-//            rs = GearPredictoO5.predict(positions.get(size-1),velocities.get(size-1),currentAcc,DT,m);
-//            currentAcc = new Pair<>(rs.get(2),0.0);
-//            positions.add(new Pair<>(rs.get(0),0.0));
-//            velocities.add(new Pair<>(rs.get(1),0.0));
-//        }
-
-
-//        VERLET
-
-        //DUDA: POSITION ANTERIOR CON EULER ? YES OR NO ?
-
-
-//        Pair<Double,Double> prevVel = Euler.prevVel(initVelocity,new Pair<>(
-//                        calcForce(initPosition.getX_value(),initVelocity.getX_value()),0.0),
-//                DT,
-//                m
-//        );
-//        Pair<Double,Double> prevPos = Euler.prevPosition(initPosition,initVelocity,new Pair<>(
-//                calcForce(initPosition.getX_value(),initVelocity.getX_value()),0.0),
-//                DT,
-//                m);
-//
-//        positions.add(0,prevPos);
-//
-//        for (double t = 0; t <=tf ; t+=DT) {
-//            int size = positions.size();
-//            positions.add(OriginalVerlet.position(positions,DT,new Pair<>(
-//                calcForce(initPosition.getX_value(),initVelocity.getX_value()),0.0),m));
-//        }
-        int a = 0;
-
-
+        rg.addStateToPositionFile(times, positions);
     }
 
     public static Double calcForce(Double pos, Double vel) {
---
+++ TP4/TP4_java/src/main/java/ar/edu/itba/simulacion/models/Methods.java
@@ -0,0 +1,6 @@
+package ar.edu.itba.simulacion.models;
+
+public enum Methods {
+
+    BEEMAN,VERLET,GEAR
+}
