f4546a593ec37a7a675dd57cf575f16eb87651d6
Santaigo Preusche <spreusche@itba.edu.ar>
2022-11-02 17:27:35 -0300

    particles vs time plot

--- TP5/TP5_python/plots/particles_vs_time.py
+++ TP5/TP5_python/plots/particles_vs_time.py
@@ -1,28 +1,29 @@
 import plotly.graph_objects as go
 
 
-def particles_vs_time_plot(particles_time: str):
-    particles_time_file = open(particles_time)
-    lines = particles_time_file.readlines()
-    time = []
-    particles = []
-    i:int = 0
-    for line in lines:
-        time.append(float(line))
-        particles.append(i)
-        i += 1
+def particles_vs_time_plot(particles_times):
+    data = []
+    for p in particles_times:
+        particles_times_file = open(p)
+        lines = particles_times_file.readlines()
+        time = []
+        particles = []
+        i:int = 0
+        for line in lines:
+            time.append(float(line))
+            particles.append(i)
+            i += 1
 
-    data = [go.Scatter(
-        y=particles[:],
-        x=time[:],
-        line=dict(color="#e9bd15"),
-        name="Cantidad de Partículas vs Tiempo",
-    )]
+        data.append(go.Scatter(
+            y=particles[:],
+            x=time[:],
+            name="Cantidad de Partículas vs Tiempo",
+        ))
 
     fig = go.Figure(
         data=data,
         layout=go.Layout(
-            xaxis=dict(title='Tiempo (s)', exponentformat="power", showgrid=False, linecolor='black',
+            xaxis=dict(title='Tiempo (s)', exponentformat="power",  showgrid=False, linecolor='black',
                        ticks='inside'),
             yaxis=dict(title='Cantidad Partículas', exponentformat="power", showgrid=False, linecolor='black',
                        ticks='inside'),
@@ -34,10 +35,14 @@ def particles_vs_time_plot(particles_time: str):
         )
     )
 
-    fig.update_layout(width=1000, height=1000)
+    fig.update_layout(width=2000, height=1000)
     fig.show()
 
 
 if __name__ == "__main__":
-    particles_time = './simulation_results/Time.txt'
-    particles_vs_time_plot(particles_time)
+    particle_times = [
+                      './simulation_results/15.000000/Times.txt',
+                      './simulation_results/20.000000/Times.txt'
+                      ]
+
+    particles_vs_time_plot(particle_times)
